9cc0bb2512073d675459e1eeef5b9c9348cf81fc
nizar <n.bahajoub@gmail.com>
2024-01-17 12:18:04 -0800

    COnfig changed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,5 +7,5 @@ sudo mkdir -p /data/web_static/shared /data/web_static/releases/test
 echo "Hello world" | sudo tee /data/web_static/releases/test/index.html > /dev/null 2>&1
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '0i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '40i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
